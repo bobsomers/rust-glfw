798c2a1abac5618b5f500e9bfa35ad9aaf9ab7f1
Bob Somers <bob@bobsomers.com>
2014-09-28 02:52:40 -0700

    GLFW version and version string.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 extern crate libc;
 
 use libc::{c_int};
+use std::c_str;
 
 mod ffi;
 mod platform;
 
-pub fn get_version() -> (i32, i32, i32) {
+pub fn version() -> (i32, i32, i32) {
     let mut major: c_int = 0;
     let mut minor: c_int = 0;
     let mut rev: c_int = 0;
@@ -13,4 +14,11 @@ pub fn get_version() -> (i32, i32, i32) {
         ffi::glfwGetVersion(&mut major, &mut minor, &mut rev);
     }
     return (major as i32, minor as i32, rev as i32);
+}
+
+pub fn version_string() -> String {
+    let version_cstr = unsafe {
+        c_str::CString::new(ffi::glfwGetVersionString(), false)
+    };
+    version_cstr.as_str().unwrap().to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate glfw;
 
 fn main() {
-    let (major, minor, rev) = glfw::get_version();
+    let (major, minor, rev) = glfw::version();
+    let version_string = glfw::version_string();
     println!("GLFW version is {}.{}.{}", major, minor, rev);
+    println!("GLFW version string is {}", version_string);
 }
